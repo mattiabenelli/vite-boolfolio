281b1100a9f42bd8e262646cc1bd3e908b3f2c57
mattiabenelli <benellimattia0@gmail.com>
2023-03-11 12:24:38 +0100

    milestone 3

---
+++ src/components/ProjectMain.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: "ProjectMain",
+  data() {
+    return {
+      projects: [],
+      loading: true,
+      baseUrl: "http://http://127.0.0.1:8000",
+    };
+  },
+  methods: {
+    getPost() {
+      this.loading = true;
+      axios.get(`${this.baseUrl}/api/projects`).then((response) => {
+        if (response.data.success) {
+          this.projects = response.data.results;
+          this.loading = false;
+        } else {
+          //failed message
+        }
+      });
+    },
+  },
+};
+</script>
+
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">Boolpress</h2>
+            </div>
+            <div class="col-12">
+                LISTA PROGETTI
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss">
+</style>
