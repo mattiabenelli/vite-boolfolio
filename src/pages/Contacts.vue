bde6f20c84ac2b0310212a783decab8bdffa42fb
mattiabenelli <benellimattia0@gmail.com>
2023-03-15 17:04:16 +0100

    problema chiamata axios/api

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,8 +1,44 @@
 <script>
+import axios from "axios";
+import { store } from "../store";
+
 export default {
   name: "Contacts",
   data() {
-    return {};
+    return {
+      store,
+      name: "",
+      surname: "",
+      email: "",
+      phone: "",
+      message: "",
+      errors: "",
+    };
+  },
+  methods: {
+    sendForm() {
+      const data = {
+        name: this.name,
+        surname: this.surname,
+        email: this.email,
+        phone: this.phone,
+        message: this.message,
+      };
+
+      axios
+        .post(`${this.store.baseUrl}/api/contacts`, data)
+        .then((response) => {
+          if (!this.success) {
+            this.errors = response.data.errors;
+          } else {
+            this.name = "";
+            this.surname = "";
+            this.email = "";
+            this.phone = "";
+            this.message = "";
+          }
+        });
+    },
   },
 };
 </script>
@@ -15,18 +51,49 @@ export default {
                 </div>
                 <div class="col-12">
                     <h5 class="text-center">Informazioni di contatto</h5>
-                    <div class="row">
+                    <div class="row my-4">
                         <div class="col-12 col-md-6">
+                            <div class="text-center">
+                                <strong></strong>
+                            </div>
                             Dati colonna sinistra
                         </div>
                         <div class="col-12 col-md-6">
-                            Dati colonna destra
+                            <div class="text-center d-flex justify-content-end">
+                                Dati colonna destra
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-12">
                     <h5 class="text-center">Scrivici</h5>
-                    <form action=""></form>
+                    <form @submit.prevent="sendForm">
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <label class="control-laber" for="nome">Nome</label>
+                                <input type="text" class="form-control" name="nome" id="nome" placeholder="Inserisci nome" v-model="name">
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <label class="control-laber" for="cognome">cognome</label>
+                                <input type="text" class="form-control" name="cognome" id="cognome" placeholder="Inserisci cognome" v-model="surname">
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <label class="control-laber" for="Email">Email</label>
+                                <input type="mail" class="form-control" name="Email" id="Email" placeholder="Inserisci Email" v-model="email">
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <label class="control-laber" for="Telefono">Telefono</label>
+                                <input type="phone" class="form-control" name="Telefono" id="Telefono" placeholder="Inserisci Telefono" v-model="phone">
+                            </div>
+                            <div class="col-12 my-2">
+                                <label class="control-laber" for="Messaggio">Messaggio</label>
+                                <textarea class="w-100 m-auto" name="messaggio" id="messaggio" cols="30" rows="10" v-model="message"></textarea>
+                            </div>
+                            <div class="col-12 my-2 text-center">
+                                <button type="submit" class="btn btn-success">invia</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
